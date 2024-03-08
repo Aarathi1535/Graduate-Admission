ddac9cf5dad760c354abf061fcb83d0f25777a57
Aarathi1535 <121624731+Aarathi1535@users.noreply.github.com>
2024-03-08 16:00:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import pandas as pd
 from sklearn.svm import SVC
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
-df = pd.read_csv("Admission_Predict.csv", error_bad_lines=False)
+df = pd.read_csv("Admission_Predict.csv")
 df1 = df.drop(['Serial No.'],axis=1)
 x = df.iloc[:,[1,2,3,4,5,6,7]].values
 y = df.iloc[:,8].values
